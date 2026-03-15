1c61223bdf53d582eefc9ce1ba4e3a419ee44230
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-03-15 19:22:04 +0200

    done

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -1,9 +1,16 @@
 package com.example.todo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Todo API", version = "1.0", description = "Documentation Todo API v1.0"), security = @SecurityRequirement(name = "bearerAuth"))
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
 public class TodoApplication {
     public static void main(String[] args) {
         SpringApplication.run(TodoApplication.class, args);
